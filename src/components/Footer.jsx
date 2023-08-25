07f4bf526813d15a9aeb624e31c8e9c1051ef917
Guillermo Alejandro Costilla <gcostilla96@gmail.com>
2023-08-25 09:46:36 -0300

    corrección footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
         <footer className="bg-white rounded-lg shadow dark:bg-gray-900">
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0">
+                    <a href="#" className="flex items-center mb-4 sm:mb-0">
                         <img src="../../public/icon/avion.png" className="h-8 mr-3" alt="Avion Logo" />
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">My Tinerary</span>
                     </a>
