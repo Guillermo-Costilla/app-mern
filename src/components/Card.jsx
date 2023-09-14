0e1b0a521988c228c23ce01276fb9e347723d251
Guillermo Alejandro Costilla <gcostilla96@gmail.com>
2023-09-14 10:03:36 -0300

    correccion

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@
 export default function Card({ city, image, country }) {
     return (
         <div className="flex justify-center items-center w-full">
-            <div className="w-[400px] h-[500px] rounded-lg bg-cover shadow-2xl m-[30px] hover:-translate-y-1 hover:scale-110 duration-300" style={{ backgroundImage: `url(${image})` }}>
+            <div className="w-[400px] h-[500px] rounded-lg bg-cover bg-center shadow-2xl m-[30px] hover:-translate-y-1 hover:scale-110 duration-300" style={{ backgroundImage: `url(${image})` }}>
                 <div className="w-full h-full flex flex-col items-center justify-end opacity-100">
                     <span className="w-full bg-black bg-opacity-50 opacity-100">
                         <h2 className="text-white text-3xl font-bold">{city}</h2>
