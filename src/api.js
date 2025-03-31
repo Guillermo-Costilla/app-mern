5ba981993c841cf5118a355bf4767a79f2acbb94
Guillermo <gcostilla96@gmail.com>
2025-03-31 19:20:58 -0300

    solución /

--- src/api.js
+++ src/api.js
@@ -1,15 +1,16 @@
 import axios from 'axios';
 
 let apiUrl = axios.create({
-  baseURL: 'http://localhost:4000/api/' // Cambia por tu URL
+  baseURL: 'http://localhost:4000/api' // Cambia por tu URL
 });
 
 // Cambia la baseURL si está en modo producción
 if (import.meta.env.MODE === 'production') {
   apiUrl = axios.create({
-    baseURL: import.meta.env.VITE_APP_BACK_URL
+    baseURL: `${import.meta.env.VITE_APP_BACK_URL}/api`
   });
 }
+console.log(import.meta.env.VITE_APP_BACK_URL)
 
 // Lista de endpoints que no requieren autenticación
 const publicEndpoints = ['cities', 'auth/signin', 'auth/signup', '']; // Agrega tus endpoints públicos aquí
